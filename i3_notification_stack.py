fd30fbb997c399f24597d28743d13f259c681587
thisistrivial <g.lee13770334@gmail.com>
2020-03-29 19:36:48 -0400

    Python file updated to alpha version and stack.p removed

--- i3_notification_stack.py
+++ i3_notification_stack.py
@@ -1,5 +1,36 @@
 from sys import argv
 from subprocess import call
+import pickle
 
-num_args = len(argv)
-call(["notify-send", argv[0]])
+def run(args):
+  try:
+    stack = open("stack.p", "rb")
+    contents = pickle.load(stack)
+    if args[0] == "push":
+      command = ["command"]
+      command.extend(args[1:])
+      if contents["stackmode"]:
+        contents["stack"].append(command)
+      else:
+        call(command)
+    elif args[0] == "toggle":
+      if contents["stackmode"]:
+        for command in contents["stack"]:
+          call(command)
+        contents["stack"] = []
+      contents["stackmode"] = not contents["stackmode"]
+    stack.close()
+    stack = open("stack.p", "wb")
+    pickle.dump(contents, stack)
+    stack.close() 
+  except:
+    initStack()
+
+def initStack():
+  stack = open("stack.p", "wb")
+  contents = {}
+  contents["stackmode"] = False
+  contents["stack"] = []
+  pickle.dump(contents, stack)
+
+run(argv)
