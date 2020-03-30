5dc6a07da3fca2ffb5ff8759f2998949d9920ffe
thisistrivial <g.lee13770334@gmail.com>
2020-03-29 20:26:35 -0400

    Updated Python file to a working version

--- i3_notification_stack.py
+++ i3_notification_stack.py
@@ -1,33 +1,36 @@
 from sys import argv
 from subprocess import call
+from os.path import dirname, abspath, exists
 import pickle
 
 def run(args):
-  try:
-    stack = open("stack.p", "rb")
-    contents = pickle.load(stack)
-    if args[0] == "push":
-      command = ["command"]
-      command.extend(args[1:])
-      if contents["stackmode"]:
-        contents["stack"].append(command)
-      else:
+  stack_file = dirname(abspath(__file__)) + "/stack.p"
+  if not exists(stack_file):
+    initStack(stack_file)
+  stack = open(stack_file, "rb")
+  contents = pickle.load(stack)
+  if args[1] == "push":
+    if contents["stackmode"]:
+      contents["stack"].append(args[2:])
+    else:
+      command = ["notify-send"]
+      command.extend(args[2:])
+      call(command)
+  elif args[1] == "toggle":
+    if contents["stackmode"]:
+      for elem in contents["stack"]:
+        command = ["notify-send"]
+        command.extend(elem)
         call(command)
-    elif args[0] == "toggle":
-      if contents["stackmode"]:
-        for command in contents["stack"]:
-          call(command)
-        contents["stack"] = []
-      contents["stackmode"] = not contents["stackmode"]
-    stack.close()
-    stack = open("stack.p", "wb")
-    pickle.dump(contents, stack)
-    stack.close() 
-  except:
-    initStack()
+      contents["stack"] = []
+    contents["stackmode"] = not contents["stackmode"]
+  stack.close()
+  stack = open(stack_file, "wb")
+  pickle.dump(contents, stack)
+  stack.close() 
 
-def initStack():
-  stack = open("stack.p", "wb")
+def initStack(stack_file):
+  stack = open(stack_file, "wb")
   contents = {}
   contents["stackmode"] = False
   contents["stack"] = []
